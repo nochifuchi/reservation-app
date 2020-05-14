49ca15abdc91bd8e5722b879cdad4710f2db1aff
Fumitaka Nozawa <fumitaka05@gmail.com>
2020-05-14 10:21:12 +0900

    Angular フロントエンド側作成

---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../../products';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+  // products: any = [1, 2, 3, 4];
+
+  products: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.products = products;
+  }
+
+}
---
+++ src/app/product/product.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-listings/product-listings.component';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
+
+const routes: Routes = [
+  {
+    path: 'products', component: ProductComponent,
+    children: [
+      { path: '', component: ProductListComponent },
+      { path: ':productId', component: ProductDetailComponent }
+    ]
+  }
+];
+
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    ProductDetailComponent,
+    ProductListComponent
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class ProductModule { }
